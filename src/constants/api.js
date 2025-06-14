5097c1da56ea6edaa46a278cb274ee9d2b7be965
PhanTruc234 <166579889+PhanTruc234@users.noreply.github.com>
2025-06-14 23:41:32 +0700

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -7,11 +7,5 @@ export const API_PRODUCT =
   "https://6848254dec44b9f3493fcc32.mockapi.io/product";
 // cart
 export const API_CART = "https://6849aba345f4c0f5ee7274f8.mockapi.io/carts";
-
-export const API_DATA_PRODUCT = "https://dummyjson.com/products";
-
-export const API_DATA_CATEGORY_LIST =
-  "https://dummyjson.com/products/category-list";
-export const API_FILTER_CATE = "https://dummyjson.com/products/category";
 export const API_PAYMENT =
   "https://684af0ef165d05c5d35aec9c.mockapi.io/payments";
