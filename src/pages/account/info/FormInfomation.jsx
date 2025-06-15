926fe67b4e1e6a489ef2e3f84fbc2bdb72e04b01
PhanTruc234 <166579889+PhanTruc234@users.noreply.github.com>
2025-06-15 14:38:01 +0700

    Update FormInfomation.jsx

--- src/pages/account/info/FormInfomation.jsx
+++ src/pages/account/info/FormInfomation.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Container,
   TextField,
@@ -14,7 +14,14 @@ export const FormInfomation = () => {
     soDienThoai: "",
     diaChi: "",
   });
-
+  const [edit, setEdit] = useState(false);
+  const [k, setK] = useState(null);
+  useEffect(() => {
+    const savedInfo = JSON.parse(localStorage.getItem("info"));
+    if (savedInfo) {
+      setFormData(savedInfo);
+    }
+  }, []);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
@@ -22,9 +29,22 @@ export const FormInfomation = () => {
       [name]: value,
     }));
   };
-
+  const handleEditInfo = () => {
+    setEdit(true);
+    const editInfo = JSON.parse(localStorage.getItem("info"));
+    if (editInfo) {
+      setFormData({
+        hoTen: editInfo.hoTen,
+        soDienThoai: editInfo.soDienThoai,
+        diaChi: editInfo.diaChi,
+      });
+    } else {
+      alert("Chưa có thông tin để sửa");
+    }
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    setEdit(false);
     console.log("Dữ liệu đã gửi:", formData);
     alert("Gửi thông tin thành công!");
     localStorage.setItem("info", JSON.stringify(formData));
@@ -39,8 +59,10 @@ export const FormInfomation = () => {
         <Box
           component="form"
           noValidate
+          required
           autoComplete="off"
           onSubmit={handleSubmit}
+          disabled={!edit}
           sx={{ display: "flex", flexDirection: "column", gap: 2 }}
         >
           <TextField
@@ -50,6 +72,7 @@ export const FormInfomation = () => {
             value={formData.hoTen}
             onChange={handleChange}
             required
+            disabled={!edit}
           />
           <TextField
             label="Số điện thoại"
@@ -58,6 +81,7 @@ export const FormInfomation = () => {
             value={formData.soDienThoai}
             onChange={handleChange}
             required
+            disabled={!edit}
             inputProps={{ pattern: "[0-9]{10,11}" }}
             placeholder="Ví dụ: 0901234567"
           />
@@ -66,13 +90,24 @@ export const FormInfomation = () => {
             variant="outlined"
             name="diaChi"
             value={formData.diaChi}
+            disabled={!edit}
             onChange={handleChange}
             required
           />
           <Button type="submit" variant="contained" color="primary">
-            Gửi thông tin
+            {edit ? "Cập nhật thông tin" : "Gửi thông tin"}
           </Button>
         </Box>
+        <Button
+          type="button"
+          variant="contained"
+          color="primary"
+          fullWidth
+          className="inline-block mt-2"
+          onClick={handleEditInfo}
+        >
+          sửa
+        </Button>
       </Paper>
     </Container>
   );
